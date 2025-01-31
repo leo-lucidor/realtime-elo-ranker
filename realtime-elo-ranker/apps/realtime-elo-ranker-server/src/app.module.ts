c4ccd823eb85e2f568ae56fa4a938ae7da72bd07
leo-lucidor <leo.lucidor@etu.univ-orleans.fr>
2025-01-31 12:03:28 +0100

    init db

--- realtime-elo-ranker/apps/realtime-elo-ranker-server/src/app.module.ts
+++ realtime-elo-ranker/apps/realtime-elo-ranker-server/src/app.module.ts
@@ -1,9 +1,20 @@
-import { Module } from '@nestjs/common';
+import { Module, OnModuleInit } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { TypeOrmModule, InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { RankingCacheService } from './services/ranking-cache/ranking-cache.service';
+import { PlayersService } from './services/players/players.service';
+import { FAKE_PLAYERS } from './data/players.data';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [RankingCacheService],
 })
